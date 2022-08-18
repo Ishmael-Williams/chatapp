0ffc43d456950481dc3023b3de2fe29406937120
Ishmael-Williams <80982790+Ishmael-Williams@users.noreply.github.com>
2022-08-18 14:45:58 -0400

    additions

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 
 import {Join} from './components/Join/Join';
-import {Chat} from './components/Chat/Chat';
+import Chat from './components/Chat/Chat';
 
 const App = () => (
     <Routes>
--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,4 +1,50 @@
 import './Chat.css';
-export function Chat(){
-    return<h1>Chat</h1>
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import {useLocation} from 'react-router-dom';
+
+let socket;
+
+
+// export function Chat({location}){
+//     const [room, setRoom] = useState('');
+//     const [name, setName] = useState('');
+//     const ENDPOINT = 'localhost:5000';
+
+//     useEffect(() =>{
+//         const {name,room} = queryString.parse(location.search);
+//         socket = io(ENDPOINT);
+//         setName(name);
+//         setRoom(room);
+//         console.log(socket);
+//     }, [ENDPOINT, location.search]);
+    
+//     return(<h1>Chat</h1>)
+// }
+
+const Chat = () =>{
+    const[name,setName] = useState('');
+    const[room,setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+    const location = useLocation();
+    useEffect(() =>{
+        const{ name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+        console.log(socket);
+        console.log(name);
+    }, [ENDPOINT, location.search]);
+
+    return (
+        <h1>Chat</h1>
+    )
 }
+
+export default Chat;
+
+
+
