e7f6d27a938fecd5adb8afefe2131ee6bc252cb0
Ishmael-Williams <80982790+Ishmael-Williams@users.noreply.github.com>
2022-08-19 14:32:25 -0400

    server changes (not working)

--- server/index.js
+++ server/index.js
@@ -2,14 +2,33 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const router = require("./router");
-
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
+
+
 // const io = Server(httpServer, {cors: {origin: "*",},});
 // const io = socketio(server, {cors: {origin: "*",},});
-app.use(router);
+
+// app.use(router);
+
+// app.use(router, function(req, res, next){
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+app.use( router,
+  cors({
+    origin: '*',
+  }), 
+  function(req,res,next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected");
