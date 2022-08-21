5d940977a16fc80fbbc057b1067d6ac44e065c54
Ishmael-Williams <80982790+Ishmael-Williams@users.noreply.github.com>
2022-08-21 12:31:32 -0400

    CORS issue resolved

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,29 +1,36 @@
 import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from "socket.io-client";
+import io from "socket.io-client"; 
 import {useLocation} from 'react-router-dom';
+import { Location } from "react-router";
+// import TextContainer from '../TextContainer/TextContainer';
+// import Messages from '../Messages/Messages';
+// import InfoBar from '../InfoBar/InfoBar';
+// import Input from '../Input/Input';
+
 import './Chat.css';
 
-
+// const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+const ENDPOINT = 'localhost:5000';
 
 let socket;
 
 const Chat = () => {
   const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
+  const [room, setRoom] = useState(''); 
+  const location = useLocation();
   // const [users, setUsers] = useState('');
   // const [message, setMessage] = useState('');
   // const [messages, setMessages] = useState([]);
-  const location = useLocation();
-  const ENDPOINT = 'localhost:5000';
+
   useEffect(() => {
+    
     const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
     setRoom(room);
-    setName(name)
-    
+    setName(name);
 
     socket.emit('join', { name, room }, (error) => {
       if(error) {
@@ -31,34 +38,15 @@ const Chat = () => {
       }
     });
   }, [ENDPOINT, location.search]);
-  
-//   useEffect(() => {
-//     socket.on('message', message => {
-//       setMessages(messages => [ ...messages, message ]);
-//     });
-    
-//     socket.on("roomData", ({ users }) => {
-//       setUsers(users);
-//     });
-// }, []);
-
-  // const sendMessage = (event) => {
-  //   event.preventDefault();
-
-  //   if(message) {
-  //     socket.emit('sendMessage', message, () => setMessage(''));
-  //   }
-  // }
 
   return (
     <div className="outerContainer">
-      {/* <div className="container">
-          <InfoBar room={room} />
+      <div className="container">
+          {/* <InfoBar room={room} />
           <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} /> */}
       </div>
-      <TextContainer users={users}/> */}
-    <h1>Chat</h1>
+      {/* <TextContainer users={users}/> */}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,47 +1,54 @@
+const http = require("http");
 const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+const socketio = require("socket.io");
+const cors = require("cors");
+
 const router = require("./router");
-const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
-
-
-// const io = Server(httpServer, {cors: {origin: "*",},});
-// const io = socketio(server, {cors: {origin: "*",},});
-
-// app.use(router);
-
-// app.use(router, function(req, res, next){
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
-app.use( router,
-  cors({
-    origin: '*',
-  }), 
-  function(req,res,next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    // origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+app.use(cors());
+app.use(router);
 
-  socket.on("join", ({name, room}) => {
-    console.log(name, room);
+io.on("connect", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    // const { error, user } = addUser({ id: socket.id, name, room });
+
+    // if (error) return callback(error);
+
+    // socket.join(user.room);
+    // socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    // socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+    // io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    // callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    // const user = getUser(socket.id);
+
+    // io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("A user has disconnected");
+    // const user = removeUser(socket.id);
+    // if(user) {
+    //   io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+    //   io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    // }
   });
 });
 
-httpServer.listen(PORT, () =>
-  console.log(`Server has started on port ${PORT}`)
+server.listen(process.env.PORT || 5000, () =>
+  console.log(`Server has started.`)
 );
